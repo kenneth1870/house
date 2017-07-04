3065cffb4f22154e0b859683fae1ef82eba7b430
Jose Chavarria <joserafaelcascante@gmail.com>
2017-07-03 22:01:04 -0600

    Upload DB Chart

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -37,7 +37,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'Resi', 'url' => ['/resi/index']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
